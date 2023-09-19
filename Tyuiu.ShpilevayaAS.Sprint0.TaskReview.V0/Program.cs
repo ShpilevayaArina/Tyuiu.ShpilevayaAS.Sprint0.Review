1cb9224fa77fa56a6141fb17f75e0033a9545be7
Tyuiu.ShpilevayaAS@outlook.com <rina.ress@mail.ru>
2023-09-19 14:40:05 +0500

    Add project files.

---
+++ Tyuiu.ShpilevayaAS.Sprint0.TaskReview.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShpilevayaAS.Sprint0.TaskReview.V0.Lib;
+
+namespace Tyuiu.ShpilevayaAS.Sprint0.TaskReview.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 5;
+            int z = 2;
+            int res = DataService.Calc(x, y, z);
+            Console.WriteLine($"({x} + {y} + {z})* 5 = " + res);
+            Console.ReadKey();
+        }
+    }
+}
